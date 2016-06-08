964c9f43062c70e8970d527c6dde822a3f866369
Michael Kearney <mkearney@ku.edu>
2016-06-08 13:43:56 -0500

    gitignore wave data function

--- wave.data.script.R
+++
@@ -1,8 +0,0 @@
-# load tfse package
-library(tfse)
-
-# calculate wave number
-wave <- round(as.numeric(Sys.Date() - as.Date("2016-05-19")) / 7, 0) + 1
-
-# run data collection script
-get_wave_data(wave)
