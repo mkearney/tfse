01279c92dea129aa8c3fa673cb0c166101f14801
Michael Kearney <mkearney@ku.edu>
2016-09-02 07:16:12 -0500

    edited readme and added full path to study script

--- README.md
+++ README.md
@@ -2,10 +2,4 @@
 tfse = **t**witter **f**ollows & **s**elective **e**xposure
 
 ## about tfse
-- This package should no longer be used to collect data via
-Twitter's API. The package has since been overhauled and constructed
-separately from my dissertation-specific functions (which was the original
-purpose behind the `tfse` repository).
-
-- [Click here to go to the updated (and better/more reliable) Twitter API package](https://github.com/mkearney/rtweet).
-
+- various functions used in my dissertation
--- study.script.R
+++ study.script.R
@@ -2,15 +2,14 @@
 ##                     data collection script                       ##
 ##------------------------------------------------------------------##
 
-# install rtweet
+# install/load rtweet
 # devtools::install_github("mkearney/rtweet")
-# load rtweet
 library(rtweet)
 
 # load twitter oauth tokens
 tokens <- get_tokens()
 
-# get sample user ids
+# read sample user ids
 o <- suppressMessages(readr::read_csv(
   "/Users/mwk/r/tfse/study/data/users_3000"))
 user_ids <- o$user_id
@@ -32,12 +31,14 @@ for (i in seq_len(max_tkn)) {
   if (is.null(r)) r <- dplyr::data_frame(ids = NA_real_)
   d[[i]] <- r
   if (i %% 15 == 0) j <- j + 1
+  if (i %% 300 == 0) message(paste0(i/30, "% completed."))
 }
 
 
 # wait if necessary
 j <- 1
-rate_limit(tokens[[j]], "friends/ids")
+wait <- rate_limit(tokens[[j]], "friends/ids")[["reset"]]
+Sys.sleep(wait[[1]] * 70)
 
 # final data collection loop
 for (i in (max_tkn + 1):3000) {
@@ -62,27 +63,39 @@ d <- dplyr::data_frame(
 d
 
 # friends list length
-hist(unlist(lapply(d$friends, function(x) length(unlist(x)))),
-  col = "gray", breaks = 30, main = "")
+fl <- unlist(lapply(d$friends, function(x) length(unlist(x))))
+hist(fl, col = "gray", breaks = 30,
+  main = "Friends", ylab = "freq",
+  xlab = "# of friends")
 
 # unique ids
-if (length(unique(d$user_id)) == 3000) {
+if (all(
+  length(unique(d$user_id)) == 3000,
+  sum(fl == 1) < 100,
+  length(unique(fl)))) {
   message("Wave data collection completed!")
 }
 
 ##------------------------------------------------------------------##
 ##                          wave path                               ##
 ##------------------------------------------------------------------##
-wave_num_path <- paste0(
-  "/Users/mwk/r/tfse/study/data/wave", max(
-    sapply(list.files("study/data")[grep("+wave",
-      list.files("study/data"))], function(x)
-        as.numeric(unlist(strsplit(x, split = "e")[[1]][2])))) + 1)
+wave_num <- max(
+    sapply(list.files("/Users/mwk/r/tfse/study/data")[grep("+wave",
+      list.files("/Users/mwk/r/tfse/study/data"))], function(x)
+        as.numeric(unlist(strsplit(x, split = "e")[[1]][2])))) + 1
 
 ##------------------------------------------------------------------##
 ##                          save data                               ##
 ##------------------------------------------------------------------##
 
-# save data
-readr::write_rds(d, wave_num_path)
-message(paste0("Data saved as ", wave_num_path, "!"))
+# save local
+readr::write_rds(d,
+  paste0("/Users/mwk/r/tfse/study/data/wave", wave_num))
+
+# save dropbox
+readr::write_rds(d,
+  paste0("/Users/mwk/Dropbox/dissrtweetion/data/wave", wave_num))
+
+message(paste0("Data saved as ",
+  paste0("/Users/mwk/r/tfse/study/data/wave", wave_num),
+  "!"))
