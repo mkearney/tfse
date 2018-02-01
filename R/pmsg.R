d29af6357fd7b4ee2c97498a0a33d49e586bc033
Michael W. Kearney <kearneymw@missouri.edu>
2018-02-01 12:06:23 -0600

    added pmsg and add_commas funs

---
+++ R/pmsg.R
@@ -0,0 +1,20 @@
+#' Paste collapse input and print as message
+#'
+#' @param ... Strings to be paste0 collapse = ""
+#' @param print Logical indicating whether to print the message (default) or return an unevaluated expression
+#' @return Either invisibly returns text of message or unevaluated expression
+#' @export
+pmsg <- function(..., print = TRUE) {
+  pmsg_(list(...), print = print)
+}
+
+
+pmsg_ <- function(dots, print = TRUE) {
+  dots <- paste(dots, collapse = "")
+  msg <- gsub("\\s{2,}", " ", dots)
+  msg <- trim_ws(msg)
+  if (print) {
+    message(msg)
+  }
+  invisible(msg)
+}
