6e3f7ddd3b803f8c98eaec5048b0caa21be1bff8
Michael W. Kearney <kearneymw@missouri.edu>
2018-03-05 17:44:17 -0600

    changed map funs to *ply names

--- R/vap.R
+++ R/vap.R
@@ -1,10 +1,10 @@
 #' @export
-lmap <- function(.x, .f) {
+lply <- function(.x, .f) {
   lapply(.x, rlang::as_closure(.f))
 }
 
 #' @export
-smap <- function(.x, .f, ...) {
+sply <- function(.x, .f, ...) {
   args <- list(...)
   if ("USE.NAMES" %in% args) {
     USE.NAMES <- args$USE.NAMES
@@ -16,7 +16,7 @@ smap <- function(.x, .f, ...) {
 
 
 #' @export
-mmap <- function(.f, ...) {
+mply <- function(.f, ...) {
   args <- list(rlang::as_closure(.f), ...)
   if (!"USE.NAMES" %in% args) {
     args$USE.NAMES <- FALSE
@@ -25,26 +25,26 @@ mmap <- function(.f, ...) {
 }
 
 #' @export
-vap_lgl <- function(.x, .f) {
+vply_lgl <- function(.x, .f) {
   vapply(.x, rlang::as_closure(.f), FUN.VALUE = logical(1), USE.NAMES = FALSE)
 }
 
 #' @export
-vap_dbl <- function(.x, .f) {
+vply_dbl <- function(.x, .f) {
   vapply(.x, rlang::as_closure(.f), FUN.VALUE = numeric(1), USE.NAMES = FALSE)
 }
 
 #' @export
-vap_int <- function(.x, .f) {
+vply_int <- function(.x, .f) {
   vapply(.x, rlang::as_closure(.f), FUN.VALUE = integer(1), USE.NAMES = FALSE)
 }
 
 #' @export
-vap_fct <- function(.x, .f) {
+vply_fct <- function(.x, .f) {
   vapply(.x, rlang::as_closure(.f), FUN.VALUE = factor(1), USE.NAMES = FALSE)
 }
 
 #' @export
-vap_chr <- function(.x, .f) {
+vply_chr <- function(.x, .f) {
   vapply(.x, rlang::as_closure(.f), FUN.VALUE = character(1), USE.NAMES = FALSE)
 }
