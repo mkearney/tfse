e18a6ce14d9e90193b852fa5b14e3c05c526fbd7
Michael W. Kearney <kearneymw@missouri.edu>
2018-07-19 13:55:41 -0500

    add shhh

---
+++ R/shhh.R
@@ -0,0 +1,10 @@
+#' execute expression quietly
+#'
+#' @param expr Expression to be evaluated without additional printing.
+#' @return Output from evaluated expression.
+#' @export
+shhh <- function(expr) {
+  capture.output(x <- suppressPackageStartupMessages(
+    suppressMessages(suppressWarnings(expr))))
+  invisible(x)
+}
