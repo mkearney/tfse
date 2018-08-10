9a75816c8bf2d01dc1438739c688f16763166036
Michael W. Kearney <kearneymw@missouri.edu>
2018-08-10 13:52:05 -0500

    export peel lists

--- R/peel_lists.R
+++ R/peel_lists.R
@@ -1,5 +1,9 @@
 
-
+#' peel lists
+#'
+#' @param x Input data
+#' @return Peeled object
+#' @export
 peel_lists <- function(x) {
   while (is.list(x) && length(x) == 1 && is.recursive(x[[1]])) {
     x <- x[[1]]
