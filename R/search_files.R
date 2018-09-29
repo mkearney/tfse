cc35ac9c6edd0f72afe9de29335b6bab7140acd8
Michael W. Kearney <kearneymw@missouri.edu>
2018-09-29 15:21:52 -0500

    fix search_files

--- R/search_files.R
+++ R/search_files.R
@@ -7,41 +7,18 @@
 #' @param path Path on which to restrict search. Defaults to current working
 #'   directory.
 #' @param recursive logical
-#' @param invert logical
-#' @param file_names only show matching file names
 #' @param all.files default false excludes dot files
 #' @return Output from terminal - file name, line number, and preview of
 #'   matching text
 #' @export
-search_files <- function(x, path = ".", recursive = TRUE, invert = FALSE,
-                         file_names = FALSE, all.files = FALSE) {
-  ignore.case <- TRUE
-  args <- "-n"
-  if (recursive) {
-    args <- paste0(args, "r")
-  }
-  if (invert) {
-    args <- paste0(args, "v")
-  }
-  if (file_names) {
-    args <- paste0(args, "l")
-  }
-  if (!all.files) {
-    dot_dirs <- grep("/\\.[^\\.]", list.dirs(recursive = recursive), value = TRUE)
-    dot_dirs <- unique(regmatches(dot_dirs, regexpr("(?<=/)\\.[^/]+", dot_dirs, perl = TRUE)))
-    dot_dirs <- paste0("{", paste(dot_dirs, collapse = ","), "}")
-    args <- paste0(args, " --exclude-dir=", dot_dirs)
-  }
-  #cmd <- paste0("grep ", args, " ", shQuote(x), " ", path, " -s")
-  #o <- tryCatch(system(cmd, intern = intern), error = function(e) "search_files-error")
-  #if (identical(o, "search_files-error")) {
+search_files <- function(x, path = ".", recursive = TRUE, all.files = FALSE) {
   f <- list.files(path, all.files = all.files, full.names = TRUE, recursive = recursive)
   i <- file.info(f)
   i <- i[i$size < 25000, ]
   f <- row.names(i)
   f <- grep("\\.png$|\\.jpeg$|\\.jpg$|\\.rda$|\\.rds$|\\.tiff$|\\.pdf", f,
     invert = TRUE, value = TRUE)
-  s <- lapply(f, extra_text_search, pat = "readlines", ignore.case = ignore.case)
+  s <- lapply(f, extra_text_search, pat = x)
   names(s) <- f
   s <- s[lengths(s) > 0]
   nms <- names(s)
@@ -67,11 +44,9 @@ extra_text_search <- function(file, pat, ignore.case = TRUE) {
   m <- paste0(#"# ", seq_along(m), ". ",
     m, collapse = "\n")
   m <- gsub("( \\{\\{\\{(?!\\{))|((?<!\\})\\}\\}\\} )", "", m, perl = TRUE)
-  if (rstudioapi::isAvailable()) {
-    m <- gsub(term, paste0("\033[39m", term, "\033[38;5;242m"), m, ignore.case = ignore.case)
-    m <- paste0("\033[38;5;242m", m, "\033[39m")
-    m <- gsub(term, paste0("\033[31m", term, "\033[39m"), m)
-  }
+  m <- gsub(term, paste0("\033[39m", term, "\033[38;5;242m"), m, ignore.case = ignore.case)
+  m <- paste0("\033[38;5;242m", m, "\033[39m")
+  m <- gsub(term, paste0("\033[31m", term, "\033[39m"), m)
   m
 }
 
