5635f0070bc48585a3732789e413cfa92f53123c
Michael W. Kearney <kearneymw@missouri.edu>
2018-09-29 15:30:17 -0500

    edit search_files

--- R/search_files.R
+++ R/search_files.R
@@ -12,12 +12,14 @@
 #'   matching text
 #' @export
 search_files <- function(x, path = ".", recursive = TRUE, all.files = FALSE) {
-  f <- list.files(path, all.files = all.files, full.names = TRUE, recursive = recursive)
+  f <- list.files(path, all.files = all.files,
+    full.names = TRUE, recursive = recursive)
   i <- file.info(f)
   i <- i[i$size < 25000, ]
   f <- row.names(i)
-  f <- grep("\\.png$|\\.jpeg$|\\.jpg$|\\.rda$|\\.rds$|\\.tiff$|\\.pdf", f,
-    invert = TRUE, value = TRUE)
+  f <- grep(
+    "\\.png$|\\.jpeg$|\\.jpg$|\\.rda$|\\.rds$|\\.tiff$|\\.pdf",
+    f, invert = TRUE, value = TRUE)
   s <- lapply(f, extra_text_search, pat = x)
   names(s) <- f
   s <- s[lengths(s) > 0]
@@ -32,19 +34,23 @@ search_files <- function(x, path = ".", recursive = TRUE, all.files = FALSE) {
 }
 
 
-extra_text_search <- function(file, pat, ignore.case = TRUE) {
-  x <- tryCatch(readlines(file), error = function(e) NULL, warning = function(w) NULL)
+extra_text_search <- function(file, pat) {
+  x <- tryCatch(readlines(file),
+    error = function(e) NULL,
+    warning = function(w) NULL)
   if (is.null(x)) return(invisible())
   term <- pat
   pat <- paste0("\\b.{0,32}", pat, ".{0,32}\\b")
-  if (!any(grepl(pat, x, ignore.case = ignore.case))) {
+  if (!any(grepl(pat, x, ignore.case = TRUE))) {
     return(invisible())
   }
-  m <- regmatches_(x, pat, drop = TRUE, ignore.case = ignore.case)
+  m <- regmatches_(x, pat, drop = TRUE, ignore.case = TRUE)
   m <- paste0(#"# ", seq_along(m), ". ",
     m, collapse = "\n")
-  m <- gsub("( \\{\\{\\{(?!\\{))|((?<!\\})\\}\\}\\} )", "", m, perl = TRUE)
-  m <- gsub(term, paste0("\033[39m", term, "\033[38;5;242m"), m, ignore.case = ignore.case)
+  m <- gsub("( \\{\\{\\{(?!\\{))|((?<!\\})\\}\\}\\} )", "",
+    m, perl = TRUE)
+  m <- gsub(term, paste0("\033[39m", term, "\033[38;5;242m"),
+    m, ignore.case = TRUE)
   m <- paste0("\033[38;5;242m", m, "\033[39m")
   m <- gsub(term, paste0("\033[31m", term, "\033[39m"), m)
   m
