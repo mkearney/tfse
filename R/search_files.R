79e6eac64503c0cb39b63d7c68d03ad6cde44128
Michael W. Kearney <kearneymw@missouri.edu>
2018-09-29 15:16:34 -0500

    fix search_files

--- R/search_files.R
+++ R/search_files.R
@@ -10,12 +10,11 @@
 #' @param invert logical
 #' @param file_names only show matching file names
 #' @param all.files default false excludes dot files
-#' @param intern logical
 #' @return Output from terminal - file name, line number, and preview of
 #'   matching text
 #' @export
 search_files <- function(x, path = ".", recursive = TRUE, invert = FALSE,
-                         file_names = FALSE, all.files = FALSE, intern = FALSE) {
+                         file_names = FALSE, all.files = FALSE) {
   args <- "-n"
   if (recursive) {
     args <- paste0(args, "r")
@@ -32,6 +31,46 @@ search_files <- function(x, path = ".", recursive = TRUE, invert = FALSE,
     dot_dirs <- paste0("{", paste(dot_dirs, collapse = ","), "}")
     args <- paste0(args, " --exclude-dir=", dot_dirs)
   }
-  cmd <- paste0("grep ", args, " ", shQuote(x), " ", path, " -s")
-  system(cmd, intern = intern)
+  #cmd <- paste0("grep ", args, " ", shQuote(x), " ", path, " -s")
+  #o <- tryCatch(system(cmd, intern = intern), error = function(e) "search_files-error")
+  #if (identical(o, "search_files-error")) {
+  f <- list.files(all.files = all.files, full.names = TRUE, recursive = recursive)
+  i <- file.info(f)
+  i <- i[i$size < 25000, ]
+  f <- row.names(i)
+  f <- grep("\\.png$|\\.jpeg$|\\.jpg$|\\.rda$|\\.rds$|\\.tiff$|\\.pdf", f,
+    invert = TRUE, value = TRUE)
+  s <- lapply(f, extra_text_search, pat = "readlines", ignore.case = ignore.case)
+  names(s) <- f
+  s <- s[lengths(s) > 0]
+  nms <- names(s)
+  s <- lapply(s, paste, collapse = "\n")
+  for (i in seq_along(s)) {
+    cat("\n# File:", nms[i], fill = TRUE)
+    txt <- paste0("  ", s[[i]])
+    txt <- gsub("\n", "\n  ", txt)
+    cat(txt, fill = TRUE)
+  }
 }
+
+
+extra_text_search <- function(file, pat, ignore.case = TRUE) {
+  x <- tryCatch(readlines(file), error = function(e) NULL, warning = function(w) NULL)
+  if (is.null(x)) return(invisible())
+  term <- pat
+  pat <- paste0("\\b.{0,32}", pat, ".{0,32}\\b")
+  if (!any(grepl(pat, x, ignore.case = ignore.case))) {
+    return(invisible())
+  }
+  m <- regmatches_(x, pat, drop = TRUE, ignore.case = ignore.case)
+  m <- paste0(#"# ", seq_along(m), ". ",
+    m, collapse = "\n")
+  m <- gsub("( \\{\\{\\{(?!\\{))|((?<!\\})\\}\\}\\} )", "", m, perl = TRUE)
+  if (rstudioapi::isAvailable()) {
+    m <- gsub(term, paste0("\033[39m", term, "\033[38;5;242m"), m, ignore.case = ignore.case)
+    m <- paste0("\033[38;5;242m", m, "\033[39m")
+    m <- gsub(term, paste0("\033[31m", term, "\033[39m"), m)
+  }
+  m
+}
+
