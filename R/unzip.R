6c530246fb204138158ee87d1f762c07f5a3cc80
Michael W. Kearney <kearneymw@missouri.edu>
2018-10-17 16:43:17 -0500

    add un_zip

---
+++ R/unzip.R
@@ -0,0 +1,14 @@
+#' Unzip files into directory
+#'
+#' Unzip archive into similarly named directory
+#'
+#' @param path Name of zipfile. Must end in ".zip"
+#' @return Creates a directory in the same folder with the same name (minus the zip part)
+#' @export
+un_zip <- function(path) {
+  stopifnot(grepl("\\.zip$", path))
+  out <- sub("\\.zip$", "", path)
+  stopifnot(!dir.exists(out))
+  dir.create(out)
+  unzip(path, exdir = out)
+}
