d6d169ab0f7228ef1cd0d0f10239cd82e8a588d8
Michael W. Kearney <kearneymw@missouri.edu>
2018-12-29 12:02:14 -0600

    add read write rds

---
+++ R/read-save-rds.R
@@ -0,0 +1,37 @@
+
+
+#' Read RDS
+#'
+#' Read serialized R data file
+#'
+#' @param path Name of .rds file
+#' @return A data object
+#' @export
+#' @family readsave
+read_RDS <- function(path) {
+  con <- gzfile(path, "rb", encoding = "UTF-8")
+  on.exit(close(con))
+  .Internal(unserialize(con, NULL))
+}
+
+#' Read RDS
+#'
+#' Read serialized R data file
+#'
+#' @param x Data object to be saved
+#' @param path Name of the file to be saved
+#' @param compress Logical indicating whether to compress data–it will take up
+#'   less space but it will be slower. Defaults to FALSE.
+#' @return Invisible data object
+#' @export
+#' @family readsave
+write_RDS <- function(x, path, compress = FALSE) {
+  if (compress) {
+    con <- gzfile(path, "wb", encoding = "UTF-8")
+  } else {
+    con <- file(path, "wb", encoding = "UTF-8")
+  }
+  on.exit(close(con), add = TRUE)
+  .Internal(serializeToConn(x, con, FALSE, NULL, NULL))
+  invisible(x)
+}
