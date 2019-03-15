a301af19292ba22233388ef003c1313a7919fda9
Michael W. Kearney <kearneymw@missouri.edu>
2019-03-15 11:37:21 -0500

    Add use_P

---
+++ R/use_P.R
@@ -0,0 +1,19 @@
+#' Use paste operators in a package
+#'
+#' Imports tfse's P and PP paste operators in a package
+#'
+#' @return Adds utils-P.R file and 'tfse' to Imports
+#' @export
+use_P <- function () {
+  if (!requireNamespace("usethis", quietly = TRUE)) {
+    stop("'use_P()' requires that you install {usethis}.", call. = FALSE)
+  }
+  usethis:::check_is_package("use_P()")
+  if (!usethis:::uses_roxygen()) {
+    stop("'use_P()' requires use of {roxygen}.", call. = FALSE)
+  }
+  usethis:::use_dependency("tfse", "Imports")
+  new <- usethis:::use_template("P.R", "R/utils-P.R", package = "tfse")
+  usethis:::todo("Run 'devtools::document()'")
+  invisible(new)
+}
